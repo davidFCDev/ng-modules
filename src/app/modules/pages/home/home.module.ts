b00d2ce529125c30cd2a373edf30bf67b715ae0d
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-25 23:15:56 +0200

    lazyload de rutas

--- src/app/modules/pages/home/home.module.ts
+++ src/app/modules/pages/home/home.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomePageComponent } from './components/home-page/home-page.component';
-
 
 @NgModule({
   declarations: [
-    HomePageComponent
   ],
   imports: [
     CommonModule,
