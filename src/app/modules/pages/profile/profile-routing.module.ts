1ea094f02012b5c37277bed74cec20ac4a5ef291
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-25 23:12:25 +0200

    componentes rutas

--- src/app/modules/pages/profile/profile-routing.module.ts
+++ src/app/modules/pages/profile/profile-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent
+    component: ProfilePageComponent
   }
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomeRoutingModule { }
+export class ProfileRoutingModule { }
