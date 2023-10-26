7e09feef3b66dbdadd28b8ec3f5b4c15f3fa23f1
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-26 16:42:10 +0200

    register form

--- src/app/modules/auth-forms/components/register-form/register-form.component.ts
+++ src/app/modules/auth-forms/components/register-form/register-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+  styleUrls: ['./register-form.component.scss'],
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements OnInit {
+  registerForm: FormGroup | undefined;
 
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.registerForm = this._formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      surname: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  submitForm = () => {
+    this._authService.register();
+  };
 }
--- src/app/modules/pages/auth/auth.module.ts
+++ src/app/modules/pages/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { AuthFormsModule } from '../../auth-forms/auth-forms.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { RegisterPageComponent } from './register-page/register-page.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    AuthFormsModule
   ]
 })
 export class AuthModule { }
